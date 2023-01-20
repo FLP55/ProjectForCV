2dfaad6e93ec721521a701b508ae874231da0a52
DenisLomaev <denislomaev1488@gmail.com>
2023-01-20 18:55:06 +0500

    create test api

--- API/test_data/data_url.py
+++ API/test_data/data_url.py
@@ -1,5 +1,5 @@
 class MainURL:
     @staticmethod
     def get_url_from_dict(key: str) -> str:
-        all_url = {"base_url": "http://172.17.1.27:8000"}
+        all_url = {"base_url": "https://dev-api.jointml.ru"}
         return all_url[key]
