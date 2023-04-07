e7b792bb41b18376a7ac77db24dd851d9c16146b
p.tiurin <bloodpoker1@gmail.com>
2023-04-07 17:03:46 +0300

    add test 1073

---
+++ API/tests/user_registration/test_api_1073_try_reg_user_with_boundary_data_password.py
@@ -0,0 +1,14 @@
+import allure
+import pytest
+
+from API.test_framework.data.email.real_email import boundary_password
+from API.test_framework.steps.steps_api import ApiSteps
+
+
+@allure.id("1073")
+@allure.title("1073.Граничные значения пароля")
+@pytest.mark.parametrize("boundary_data", boundary_password)
+def test_api_1073_try_reg_user_with_boundary_data_password(boundary_data):
+    ApiSteps().check_status_code_after_register_with_invalid_data(
+        email="Pavel123@mail.ru", password=boundary_data, confirm_password=boundary_data
+    )
